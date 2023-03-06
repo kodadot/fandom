f3c562807b6057cf56be40697bfdd49f800377ec
Viki Val <viktorko99@gmail.com>
2023-03-06 14:21:46 +0100

    :zap: use original graphql

--- utils/shopify.ts
+++ utils/shopify.ts
@@ -1,4 +1,5 @@
 import { ask } from 'https://esm.sh/@kodadot1/uniquery@0.1.0-rc.0'
+// import { getClient } from 'https://esm.sh/@kodadot1/uniquery@0.1.1-rc.0'
 
 const ADDRESS = Deno.env.get("ADDRESS");
 const CHAIN = Deno.env.get("CHAIN");
@@ -18,27 +19,26 @@ export async function getItem<T>(id: string) {
 }
 
 export async function graphql<T>(
-  _query: string,
-  _variables: Record<string, unknown> = {},
+  query: string,
+  variables: Record<string, unknown> = {},
 ): Promise<T> {
-  // const resp = await fetch(`https://${SHOPIFY_SHOP}/api/2022-04/graphql.json`, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN!,
-  //   },
-  //   body: JSON.stringify({ query, variables }),
-  // });
-  // if (!resp.ok) {
-  //   const body = await resp.text();
-  //   throw new Error(`${resp.status} ${body}`);
-  // }
-  // const json = await resp.json();
-  // if (json.errors) {
-  //   throw new Error(json.errors.map((e: Error) => e.message).join("\n"));
-  // }
-  // return json.data as T;
+  const resp = await fetch(CHAIN!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+  if (!resp.ok) {
+    const body = await resp.text();
+    throw new Error(`${resp.status} ${body}`);
+  }
+  const json = await resp.json();
+  if (json.errors) {
+    throw new Error(json.errors.map((e: Error) => e.message).join("\n"));
+  }
+  return json.data as T;
   // Sleep for 1 second
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-  return {} as T
+  // await new Promise((resolve) => setTimeout(resolve, 1000))
+  // return {} as T
 }
