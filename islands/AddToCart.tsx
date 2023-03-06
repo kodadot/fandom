b36ed82b5d213a63f716398a9c749acbec036a51
Viki Val <viktorko99@gmail.com>
2023-03-06 14:57:15 +0100

    :zap: add to cart visits KodaDot

--- islands/AddToCart.tsx
+++ islands/AddToCart.tsx
@@ -1,31 +1,20 @@
-import { useState } from "preact/hooks";
-import { addToCart, useCart } from "@/utils/data.ts";
-
 interface AddToCartProps {
   id: string;
 }
 
 export default function AddToCart(props: AddToCartProps) {
-  const { data } = useCart();
-  const [isAdding, setIsAdding] = useState(false);
-
   const add = (e: MouseEvent) => {
     e.preventDefault();
-    setIsAdding(true);
-    addToCart(data!.id, props.id).finally(() => {
-      setIsAdding(false);
-    });
+    const url = `https://kodadot.xyz/bsx/gallery/${props.id}`;
+    window.open(url, '_blank')
   };
 
   return (
     <button
       onClick={add}
-      disabled={!data && !isAdding}
-      class={`w-full ${
-        isAdding ? "!bg-gray-400" : "bg-gray-700"
-      } border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-900`}
+      class={`w-full bg-gray-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-900`}
     >
-      {isAdding ? "Adding..." : "Add to Cart"}
+      Visit on Kodadot
     </button>
   );
 }
