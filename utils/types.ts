ade89a5212e456e288e6a4770ee08bfa6e085878
Viki Val <viktorko99@gmail.com>
2023-03-09 15:44:12 +0100

    :wastebasket: :label: useless types

--- utils/types.ts
+++ utils/types.ts
@@ -1,24 +1,3 @@
-export interface Money {
-  amount: number;
-  currencyCode: string;
-}
-
-export interface Image {
-  url: string;
-  width: number;
-  height: number;
-  altText: string;
-}
-
-export interface List<T> {
-  nodes: T[];
-}
-
-export interface ProductPriceRange {
-  minVariantPrice: Money;
-  maxVariantPrice: Money;
-}
-// ['id', 'createdAt', 'name', 'metadata', 'currentOwner', 'issuer']
 export interface Item {
   id: string;
   createdAt: Date;
@@ -37,24 +16,4 @@ export interface Metadata {
   image: string;
   external_url?: string;
   animation_url?: string;
-}
-
-export interface Product {
-  id: string;
-  handle: string;
-  title: string;
-  description: string;
-  descriptionHtml: string;
-  productType: string;
-  featuredImage: Image | null;
-  images?: List<Image>;
-  variants: List<ProductVariant>;
-  priceRange: ProductPriceRange;
-}
-
-export interface ProductVariant {
-  id: string;
-  priceV2: Money;
-  title: string;
-  availableForSale: boolean;
 }
