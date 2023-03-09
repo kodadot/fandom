12307b5ac33b0415b0258b3fa53987f55e87aa35
Viki Val <viktorko99@gmail.com>
2023-03-09 15:07:32 +0100

    :zap: cart as stargazers count

---
+++ islands/Cart.tsx
@@ -0,0 +1,32 @@
+import IconCart from "@/components/IconCart.tsx"
+import {
+  useStargazers
+} from "@/utils/data.ts"
+
+export default function Cart() {
+  const { data, error } = useStargazers();
+
+  const add = (e: MouseEvent) => {
+    e.preventDefault();
+    const url = `https://github.com/kodadot/nft-gallery/`;
+    window.open(url, '_blank')
+  };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <button
+        onClick={add}
+        class="flex items-center gap-2 items-center border-2 border-gray-800 rounded-full px-5 py-1 font-semibold text-gray-800 hover:bg-gray-800 hover:text-white transition-colors duration-300"
+      >
+        <IconCart />
+        {data?.watchers ?? "0"}
+      </button>
+
+    </div>
+  );
+}
+
