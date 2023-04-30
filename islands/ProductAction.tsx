292bf05c0cf5e9defbd44e38c427619e85c70a51
Viki Val <viktorko99@gmail.com>
2023-04-30 21:04:16 +0200

    :zap: replace button with a link

--- islands/ProductAction.tsx
+++ islands/ProductAction.tsx
@@ -3,18 +3,13 @@ interface ProductActionProps {
 }
 
 export default function ProductAction(props: ProductActionProps) {
-  const add = (e: MouseEvent) => {
-    e.preventDefault();
-    const url = `https://kodadot.xyz/bsx/gallery/${props.id}`;
-    window.open(url, '_blank')
-  };
-
   return (
-    <button
-      onClick={add}
+    <a
+      href={`https://kodadot.xyz/bsx/gallery/${props.id}`}
       class={`w-full bg-gray-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-900`}
+      target={'_blank'}
     >
       Visit on Kodadot
-    </button>
+    </a>
   );
 }
