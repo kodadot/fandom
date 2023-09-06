36a3a6efe5d2e1cd6e76880a453efc5072a21839
Viki Val <viktorko99@gmail.com>
2023-09-06 12:31:17 +0200

    :zap: use CHAIN env variable

--- islands/ProductAction.tsx
+++ islands/ProductAction.tsx
@@ -1,3 +1,5 @@
+import { CHAIN } from "@/utils/indexer.ts";
+
 interface ProductActionProps {
   id: string;
 }
@@ -5,7 +7,7 @@ interface ProductActionProps {
 export default function ProductAction(props: ProductActionProps) {
   return (
     <a
-      href={`https://kodadot.xyz/ahk/gallery/${props.id}`}
+      href={`https://kodadot.xyz/${CHAIN}/gallery/${props.id}`}
       class={`w-full bg-gray-700 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-gray-900`}
       target={'_blank'}
     >
